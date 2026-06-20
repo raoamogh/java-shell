53c1ffc18784d73fb27e248d95892dd756e859c9
raoamogh <amoghagrao@gmail.com>
2026-06-20 13:15:09 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,59 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static String findCmd(String cmd){
+        String path = System.getenv("PATH");
+
+        String[] dirs = path.split(":");
+
+        for(String dir : dirs){
+            File file = new File(dir, cmd);
+
+            if(file.exists()){
+                return file.getAbsolutePath();
+            }
+        }
+
+        return null;
+    }
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        while (true) {
+        while(true){
             System.out.print("$ ");
-
             String input = sc.nextLine();
-
-            String cmd = input.indexOf(" ") == -1 ? input : input.substring(0, input.indexOf(" "));
-            String rem = input.indexOf(" ") == -1 ? "" : input.substring(input.indexOf(" ") + 1);
-
-            if (cmd.equals("exit")) {
+            if(input.equals("exit")){
                 break;
-            } else if (cmd.equals("type")) {
-                if (rem.equals("exit") || rem.equals("echo") || rem.equals("type"))
-                    System.out.println(rem + " is a shell builtin");
-                else System.out.println(rem + ": not found");
-            } else if (cmd.equals("echo")) {
-                System.out.println(rem);
+            }
+
+            String[] parts = input.split(" ");
+
+            if(parts[0].equals("echo")){
+                for(int i = 1; i < parts.length; i++){
+                    System.out.print(parts[i]);
+
+                    if(i != parts.length - 1){
+                        System.out.print(" ");
+                    }
+                }
+
+                System.out.println();
+            } else if(parts[0].equals("type")){
+                String cmd = parts[1];
+
+                if(cmd.equals("echo") || cmd.equals("exit") || cmd.equals("type")){
+                    System.out.println(cmd + " is a shell builtin");
+                } else {
+                    String loc = findCmd(cmd);
+
+                    if(loc != null){
+                        System.out.println(cmd + " is " + loc);
+                    } else {
+                        System.out.println(cmd + ": not found");
+                    }
+                }
             } else {
-                System.out.println(cmd + ": command not found");
+                System.out.println(input + ": command not found");
             }
         }
     }
