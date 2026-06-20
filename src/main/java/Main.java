d763073623504d758ef0608d8e0006efae5e616f
raoamogh <amoghagrao@gmail.com>
2026-06-20 16:12:12 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
+import java.io.PrintStream;
+import java.io.FileOutputStream;
 
 public class Main {
     public static String findCmd(String cmd){
@@ -61,38 +63,58 @@ public class Main {
         while(true){
             System.out.print("$ ");
             String input = sc.nextLine();
+            PrintStream out = System.out;
+
             if(input.equals("exit")){
                 break;
             }
 
             List<String> parts = parseCmd(input);
+            String outputFile = null;
+
+            for(int i = 0; i < parts.size(); i++){
+                if(parts.get(i).equals(">") || parts.get(i).equals("1>")){
+                    outputFile = parts.get(i+1);
+                    parts = new ArrayList<>(parts.subList(0, i));
+                    break;
+                }
+            }
+
+            if(outputFile != null){
+                try{
+                    out = new PrintStream(new FileOutputStream(outputFile));
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+                
+            }
 
             if(parts.get(0).equals("echo")){
                 for(int i = 1; i < parts.size(); i++){
-                    System.out.print(parts.get(i));
+                    out.print(parts.get(i));
 
                     if(i != parts.size() - 1){
-                        System.out.print(" ");
+                        out.print(" ");
                     }
                 }
 
-                System.out.println();
+                out.println();
             } else if(parts.get(0).equals("type")){
                 String cmd = parts.get(1);
 
                 if(cmd.equals("echo") || cmd.equals("exit") || cmd.equals("type") || cmd.equals("pwd")){
-                    System.out.println(cmd + " is a shell builtin");
+                    out.println(cmd + " is a shell builtin");
                 } else {
                     String loc = findCmd(cmd);
 
                     if(loc != null){
-                        System.out.println(cmd + " is " + loc);
+                        out.println(cmd + " is " + loc);
                     } else {
-                        System.out.println(cmd + ": not found");
+                        out.println(cmd + ": not found");
                     }
                 }
             } else if(parts.get(0).equals("pwd")){
-                System.out.println(currDir);
+                out.println(currDir);
             } else if(parts.get(0).equals("cd")){
                 File dir;
                 if(parts.get(1).equals("~")){
@@ -110,7 +132,7 @@ public class Main {
                         e.printStackTrace();
                     }
                 } else {
-                    System.out.println("cd: " + parts.get(1) + ": No such file or directory");
+                    out.println("cd: " + parts.get(1) + ": No such file or directory");
                 }
             } else {
                 String exec = findCmd(parts.get(0));
@@ -118,13 +140,16 @@ public class Main {
                     try{
                         ProcessBuilder pb = new ProcessBuilder(parts);
                         pb.inheritIO();
+                        if(outputFile != null){
+                            pb.redirectOutput(new File(outputFile));
+                        }
                         Process process = pb.start();
                         process.waitFor();
                     } catch (Exception e){
                         e.printStackTrace();
                     }
                 } else {
-                    System.out.println(input + ": command not found");
+                    out.println(input + ": command not found");
                 }
             } 
         }
